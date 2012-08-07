842179ab577ed86b0d814e66321a899f4a008662
marccruz <mcruz@serranomobile.com>
2012-08-07 03:27:36 -0700

    Initial commit for async add

--- web/src/main/java/com/serranomobile/sandbox/web/controllers/HomeController.java
+++ web/src/main/java/com/serranomobile/sandbox/web/controllers/HomeController.java
@@ -2,35 +2,49 @@ package com.serranomobile.sandbox.web.controllers;
 
 import java.util.ArrayList;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.serranomobile.sandbox.web.domains.User;
 
-@RequestMapping("/")
+@RequestMapping("/web")
 @Controller
 public class HomeController {
-    @RequestMapping public String index(Model uiModel) {
+	
+    @RequestMapping(method=RequestMethod.GET)
+    public String index(Model uiModel) {
+    	ArrayList<User> users = getAllUsers();
+    	uiModel.addAttribute("users", users);
+    	return "users/list";
+    }
+
+    @RequestMapping(value="/users?form", method=RequestMethod.GET)
+    public String createForm(@ModelAttribute User u, Model uiModel) {
+    	if (u == null) {
+    		u = new User();
+    	}
+    	uiModel.addAttribute("user", u);
+    	return "users/create";
+    }
+    
+    private ArrayList<User> getAllUsers() {
     	ArrayList<User> users = new ArrayList<User>();
     	User u = new User();
-    	u.setAccountNumber("CRUZMC*216ME");
+    	u.setAccountNumber("CRUZMC216ME");
     	u.setFirstName("Marc");
     	u.setLastName("Cruz");
     	users.add(u);
     	
     	u = new User();
-    	u.setAccountNumber("BELL*N216ME");
+    	u.setAccountNumber("BELLN216ME");
     	u.setFirstName("Noah");
     	u.setLastName("Bell");
     	users.add(u);
     	
-    	uiModel.addAttribute("users", users);
-        return "index";
+    	return users;
     }
+
 }
