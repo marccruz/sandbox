6b09e2125c03d7dc63e27fd42961f4a8ed159fb0
marccruz <mcruz@serranomobile.com>
2012-09-04 07:24:44 -0700

    Using dataTable

--- confirmation/src/main/java/com/serranomobile/sandbox/web/UserController.java
+++ confirmation/src/main/java/com/serranomobile/sandbox/web/UserController.java
@@ -1,12 +1,17 @@
 package com.serranomobile.sandbox.web;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.serranomobile.sandbox.domains.User;
 
 @RequestMapping("/user/**")
 @Controller
@@ -16,8 +21,17 @@ public class UserController {
     public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
     }
 
-    @RequestMapping
-    public String index() {
-        return "user/index";
+    @RequestMapping(method=RequestMethod.GET)
+    public String list(Model uiModel) {
+    	ArrayList<User> users = new ArrayList<User>();
+    	User u = new User();
+    	u.setName("marc");
+    	u.setPassword("password");
+    	users.add(u);
+    	u = new User();
+    	u.setName("noah");
+    	users.add(u);
+    	uiModel.addAttribute("users", users);
+        return "user/list";
     }
 }
